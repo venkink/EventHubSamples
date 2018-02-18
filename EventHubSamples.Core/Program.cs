1e7bcdbb5270207667dd355d260bec174a34e59e
Venkatesh <venkatesh.nls@gmail.com>
2018-02-18 15:56:44 +0530

    initial draft

--- EventHubSamples.Core/Program.cs
+++ EventHubSamples.Core/Program.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using Microsoft.Azure.EventHubs;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EventHubSamples.Core
 {
     class Program
     {
+        private static EventHubClient eventHubClient;
+        private const string EhConnectionString = "Endpoint=sb://hon-eventhub.servicebus.windows.net/;SharedAccessKeyName=send;SharedAccessKey=A5cL9Qp1GdRYit/IG+Y/KAOZAtTo4JBV9JVDpegoQbw=";
+        private const string EhEntityPath = "send";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+        private static async Task MainAsync(string[] args)
+        {
+            // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
+            // Typically, the connection string should have the entity path in it, but this simple scenario
+            // uses the connection string from the namespace.
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EhConnectionString)
+            {
+                EntityPath = EhEntityPath
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+            while (true)
+            {
+                Console.WriteLine("Enter Message to send");
+                string message = Console.ReadLine();
+                if (message == "stop")
+                {
+                    break;
+                }
+                await SendMessagesToEventHub(message);
+            }
+            await eventHubClient.CloseAsync();
+
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+        private static async Task SendMessagesToEventHub(string message)
+        {
+            //for (var i = 0; i < numMessagesToSend; i++)
+            //{
+            try
+            {
+                //      var message = $"Message {i}";
+                Console.WriteLine($"Sending message: {message}");
+                await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+            }
+
+         //   await Task.Delay(10);
+            //  }
+
+            Console.WriteLine($"{message} messages sent.");
         }
     }
 }
---
+++ EventHubSamples.Receiver/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubSamples.Receiver
+{
+    class Program
+    {
+        private const string EhConnectionString = "Endpoint=sb://hon-eventhub.servicebus.windows.net/;SharedAccessKeyName=listen;SharedAccessKey=M6AHpRFsdqAb5jNSrOtXNmNre8WFwFzgl9r0lD1DYAM=";
+        private const string EhEntityPath = "send";
+        private const string StorageContainerName = "sample";
+        private const string StorageAccountName = "honeventhubdemo";
+        private const string StorageAccountKey = "OzrQMTnl2xCLnhcCit0x+gFvFuQ4Wm1rsiBrPIYltwiToU2AZI7zuScayb78ljGAGopJKJhn/hseT+GT5pJqoQ==";
+
+        private static readonly string StorageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EhEntityPath,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EhConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+    public class SimpleEventProcessor : IEventProcessor
+    {
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine($"Processor Shutting Down. Partition '{context.PartitionId}', Reason: '{reason}'.");
+            return Task.CompletedTask;
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine($"SimpleEventProcessor initialized. Partition: '{context.PartitionId}'");
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessErrorAsync(PartitionContext context, Exception error)
+        {
+            Console.WriteLine($"Error on Partition: {context.PartitionId}, Error: {error.Message}");
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (var eventData in messages)
+            {
+                var data = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                Console.WriteLine($"Message received. Partition: '{context.PartitionId}', Data: '{data}'");
+            }
+
+            return context.CheckpointAsync();
+        }
+    }
+}
